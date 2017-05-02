c4086b55038cb09fc21f887bbd2277b8b881c12a
Kiran Challa <kichalla@microsoft.com>
2017-05-01 22:45:21 -0700

    Initial

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace foo
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+             app.Run(async context =>
+            {
+                context.Response.ContentType = "text/plain";
+                await context.Response.WriteAsync("Hello world");
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
